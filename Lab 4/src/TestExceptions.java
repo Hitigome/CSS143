96dc98e1ce84fb759b5d6186f279e4d59d28f894
Hitigome <kawibrian1@gmail.com>
2023-02-08 21:41:03 -0800

    Changed Exception to IOException and moved try to cover other files

--- Lab 4/src/TestExceptions.java
+++ Lab 4/src/TestExceptions.java
@@ -1,14 +1,14 @@
 import java.io.*;
 public class TestExceptions{
     public static void main(String[] args){
-
+        try{
         //Open the Input file
         File inputFile = new File("Sample.jpg");
         //Open the Output file
         File outputFile1 = new File("Output1.jpg");
 
         //Get file handlers in Byte Stream format
-        try{
+        
             FileInputStream in1 = new FileInputStream(inputFile);
             FileOutputStream out1 = new FileOutputStream(outputFile1);
        
@@ -21,7 +21,7 @@ public class TestExceptions{
         //Close the files
             in1.close();
             out1.close();
-        }catch(Exception e){
+        }catch(IOException e){
             System.out.println("The file you have requested cannot be found");
         }
     }
