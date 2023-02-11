0a2e569073e9e3d15ef51c031dec32f619fd67ef
Hitigome <kawibrian1@gmail.com>
2023-02-11 01:40:49 -0800

    override calculateWeeklyPay to include bonus

--- Lab 5/src/PermanentHire.java
+++ Lab 5/src/PermanentHire.java
@@ -1,9 +1,16 @@
 public class PermanentHire extends SalariedWorker{
+    private int bonus;
+
     PermanentHire(String name, int social){
 
     }
 
     PermanentHire(String name, int social, double monthlyPay){
 
+    }
+
+    @Override
+    public double calculateWeeklyPay(){
+        return super.calculateWeeklyPay() + bonus;
     }
 }
