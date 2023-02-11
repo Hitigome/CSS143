cb33d0dc485419710443bff7fa8972d91d981546
Hitigome <kawibrian1@gmail.com>
2023-02-11 01:18:56 -0800

    Define getters and setters

--- Lab 5/src/ColorWithAlpha.java
+++ Lab 5/src/ColorWithAlpha.java
@@ -1,3 +1,16 @@
 public class ColorWithAlpha extends SimpleColor{
+    private int alpha;
+
+    public int getAlpha(){
+        return alpha;
+    }
+
+    public void setAlpha(int alpha){
+        if(alpha>0 && alpha<255){
+            this.alpha = alpha;
+        }else{
+            throw new ColorException();
+        }
+    }
     
 }
