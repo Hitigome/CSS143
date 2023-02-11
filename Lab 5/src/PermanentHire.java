e674f2cbbc5a1e873608e12f0501538f72cfcd25
Hitigome <kawibrian1@gmail.com>
2023-02-11 01:35:28 -0800

    Build Constructors

--- Lab 5/src/PermanentHire.java
+++ Lab 5/src/PermanentHire.java
@@ -1,3 +1,9 @@
 public class PermanentHire extends SalariedWorker{
-    
+    PermanentHire(String name, int social){
+
+    }
+
+    PermanentHire(String name, int social, double monthlyPay){
+
+    }
 }
