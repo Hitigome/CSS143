b11f4f37c9b971304f04ad1f5b9ae7ffad7e8927
Hitigome <kawibrian1@gmail.com>
2023-02-11 00:49:05 -0800

    Build and overload constructors

--- Lab 5/src/ColorException.java
+++ Lab 5/src/ColorException.java
@@ -1,3 +1,9 @@
 public class ColorException extends RuntimeException{
+    ColorException(){
+        super("An error occured in Color");
+    }
 
+    ColorException(String msg){
+        a.super(msg);
+    }
 }
