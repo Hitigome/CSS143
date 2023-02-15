95db0f5c05072ceff744d30259af6aefa01ed8d6
Hitigome <kawibrian1@gmail.com>
2023-02-15 15:52:58 -0800

    finish constructors

--- Lab 5/src/Consultant.java
+++ Lab 5/src/Consultant.java
@@ -2,11 +2,11 @@ public class Consultant extends HourlyWorker{
     private double hourlyPay = 30.0;
 
     Consultant(String name, int social){
-
+        super(name, social);
     }
 
     Consultant(String name, int social, double monthlyPay){
-
+        super(name, social, monthlyPay);
     }
 
     @Override
