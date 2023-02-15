e8c9afee41fabf695c2bcd825fa899230641aaec
Hitigome <kawibrian1@gmail.com>
2023-02-15 14:29:59 -0800

    created new instanced variables

--- Lab 5/src/PermanentHire.java
+++ Lab 5/src/PermanentHire.java
@@ -1,16 +1,18 @@
 public class PermanentHire extends SalariedWorker{
     private int bonus;
+    private double monthlyPay = calculateWeeklyPay() * 4;
 
     PermanentHire(String name, int social){
 
     }
 
     PermanentHire(String name, int social, double monthlyPay){
-
+        super(name, social, monthlyPay);
     }
 
     @Override
     public double calculateWeeklyPay(){
         return super.calculateWeeklyPay() + bonus;
     }
+
 }
