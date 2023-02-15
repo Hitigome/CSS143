82ebc244c41093962d8a9f57819cab3e56cac634
Hitigome <kawibrian1@gmail.com>
2023-02-15 15:14:58 -0800

    create constructors, override weekly pay

--- Lab 5/src/Consultant.java
+++ Lab 5/src/Consultant.java
@@ -1,4 +1,17 @@
 public class Consultant extends HourlyWorker{
-    
+    private double hourlyPay = 30.0;
+
+    Consultant(String name, int social){
+
+    }
+
+    Consultant(String name, int social, double monthlyPay){
+
+    }
+
+    @Override
+    public double calculateWeeklyPay(){
+        return hourlyPay * 20;
+    }
     
 }
