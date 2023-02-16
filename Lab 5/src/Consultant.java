8d82cd9dd01c4724bc0872a921bea985068b8fe4
Hitigome <kawibrian1@gmail.com>
2023-02-15 16:07:39 -0800

    fix constructors

--- Lab 5/src/Consultant.java
+++ Lab 5/src/Consultant.java
@@ -1,12 +1,17 @@
 public class Consultant extends HourlyWorker{
-    private double hourlyPay = 30.0;
+    private double hourlyPay;
+    private double minimum = 30.0;
 
     Consultant(String name, int social){
         super(name, social);
+        hourlyPay = minimum;
     }
 
-    Consultant(String name, int social, double monthlyPay){
-        super(name, social, monthlyPay);
+    Consultant(String name, int social, double pay){
+        super(name, social);
+        if(pay > 0.0){
+            hourlyPay = pay;
+        }
     }
 
     @Override
