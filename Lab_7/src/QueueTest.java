44757f39a0680c9e16507ce8191d16dbc46619eb
Hitigome <kawibrian1@gmail.com>
2023-02-20 16:04:53 -0800

    Finish driver for Queue

--- Lab_7/src/QueueTest.java
+++ Lab_7/src/QueueTest.java
@@ -1,3 +1,19 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class QueueTest {
-    
+    public static void main(String[] args) {
+        Queue<Character> q = new LinkedList<>();
+        q.add('R');
+        q.add('a');
+        q.add('c');
+        q.add('e');
+        //q.add('c');
+        //q.add('a');
+        q.add('r');
+        System.out.println("Size : " + q.size());
+        while(!q.isEmpty()) {
+          System.out.println(q.remove());
+        }
+      }
 }
